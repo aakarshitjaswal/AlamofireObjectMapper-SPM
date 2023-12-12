599ccbabfaa1ca72876edbbdf1ab28d8a2f315f9
aakarshitjaswal <aakarshitjaswal@gmail.com>
2023-12-12 16:47:07 +0530

    Fix Package Manifest

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
             name: "SPMDemo",
             targets: ["SPMDemo"]),
     ],
-    dependencies: [.package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0"),
-                   .package(url: "https://github.com/tristanhimmelman/ObjectMapper.git", from: "3,5,1")],
+    dependencies: [.package(url: "https://github.com/Alamofire/Alamofire.git", from: .init("5.0.0")!),
+                   .package(url: "https://github.com/tristanhimmelman/ObjectMapper.git", from: .init("3.5.1")!)],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
